b3fd1ef00cf6a474c3dcd3aab7616e0b3321ce01
vimalb <vimal@alum.mit.edu>
2020-06-08 20:24:36 -0400

    Initial commit

---
+++ cameraon.js
@@ -0,0 +1,16 @@
+const Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+
+const FOCUS = new Gpio(12, 'out'); //use GPIO pin 4, and specify that it is output
+const SHUTTER = new Gpio(76, 'out'); //use GPIO pin 4, and specify that it is output
+const POWER = new Gpio(200, 'out'); //use GPIO pin 4, and specify that it is output
+
+const wait = (ms) => new Promise(res => setTimeout(res, ms));
+
+
+(async () => {
+  wait(250);
+  POWER.writeSync(1);
+  FOCUS.writeSync(1);
+  SHUTTER.writeSync(1);
+  wait(250);
+})();
